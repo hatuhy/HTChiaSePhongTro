b6ebefb603b8360af189e73f19515972475a6ad3
HaTu <hatu1998hy@gmail.com>
2019-11-09 23:51:12 +0700

    xóa tìm kiếm

--- HTCSPT/resources/views/Viewer/Page/danhsach.blade.php
+++ HTCSPT/resources/views/Viewer/Page/danhsach.blade.php
@@ -44,10 +44,65 @@
                         class="ion-ios-search mr-3"></span> Tìm kiếm
                 </h2>
             </div>
-            <section class="ftco-search d-none">
+            <!-- <section class="ftco-search d-none">
                 <div class="s010">
                     <form>
                         <div class="inner-form">
+                            <div class="basic-search">
+                                <div class="input-field">
+                                    <input id="search" type="text" placeholder="Type Keywords" />
+                                    <div class="icon-wrap">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            viewBox="0 0 24 24">
+                                            <path
+                                                d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="advance-search">
+                                <span class="desc">ADVANCED SEARCH</span>
+                                <div class="row">
+                                    <div class="input-field">
+                                        <div class="input-select">
+                                            <select data-trigger="" name="choices-single-defaul">
+                                                <option placeholder="" value="">Accessories</option>
+                                                <option>Subject b</option>
+                                                <option>Subject c</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="input-field">
+                                        <div class="input-select">
+                                            <select data-trigger="" name="choices-single-defaul">
+                                                <option placeholder="" value="">Color</option>
+                                                <option>Subject b</option>
+                                                <option>Subject c</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="input-field">
+                                        <div class="input-select">
+                                            <select data-trigger="" name="choices-single-defaul">
+                                                <option placeholder="" value="">Size</option>
+                                                <option>Subject b</option>
+                                                <option>Subject c</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row second">
+                                    <div class="input-field">
+                                        <div class="input-select">
+                                            <select data-trigger="" name="choices-single-defaul">
+                                                <option placeholder="" value="">Sale</option>
+                                                <option>Subject b</option>
+                                                <option>Subject c</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="input-field">
                             <div class="basic-search">
                                 <div class="input-field">
                                     <input id="search" type="text" placeholder="Type Keywords" />
@@ -138,7 +193,7 @@
                         </div>
                     </form>
                 </div>
-            </section>
+            </section> -->
             <div class="col-md-12 lign-items-center">
                 <div class="tab-content ftco-animate" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-0" role="tabpanel"
