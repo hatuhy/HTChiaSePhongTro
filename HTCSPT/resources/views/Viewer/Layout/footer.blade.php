5865ba059a7089498b1a5f5ed8a101131078b18e
HaTu <hatu1998hy@gmail.com>
2019-11-10 17:04:38 +0700

    footer

--- HTCSPT/resources/views/Viewer/Layout/footer.blade.php
+++ HTCSPT/resources/views/Viewer/Layout/footer.blade.php
@@ -1,19 +1,19 @@
 <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <div class="container">
+    <div class="container">
         <div class="row mb-5">
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">CSPT</h2>
-              <p>Uy tín - tiện lợi - đơn giản.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">CSPT</h2>
+                    <p>Uy tín - tiện lợi - đơn giản.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
             </div>
-          </div>
-         
-         <!-- <div class="col-md">
+
+            <!-- <div class="col-md">
             <div class="ftco-footer-widget mb-4 ml-md-5">
               <h2 class="ftco-heading-2">Buy</h2>
               <ul class="list-unstyled">
@@ -38,26 +38,39 @@
             </div>
           </div>
           !-->
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            	<h2 class="ftco-heading-2">Liên hệ</h2>
-            	<div class="block-23 mb-3">
-	              <ul>
-	                <li><span class="icon icon-map-marker"></span><span class="text">Minh khai - Bắc Từ Liêm - Hà Nội</span></li>
-	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">0382544996</span></a></li>
-	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">dungpt2805@gmail.com</span></a></li>
-	              </ul>
-	            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Liên hệ</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">Minh khai - Bắc Từ Liêm -
+                                    Hà Nội</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span
+                                        class="text">0382544996</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span
+                                        class="text">dungpt2805@gmail.com</span></a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
         <div class="row">
-          <div class="col-md-12 text-center">
+            <div class="col-md-12 text-center" style="margin-left: 142px;">
 
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-          </div>
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>
+                        document.write(new Date().getFullYear());
+
+                    </script> Thuetro247.com
+                    <span>
+                        All rights reserved | Nhóm 2-KTPM1 <i class="icon-heart color-danger" aria-hidden="true"></i> Môn Phát Triển Phần Mềm Hướng Dịch Vụ
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+               
+                </span>
+                <span>Hà Văn Tú - Phạm Thị Dung - Nguyễn Tiến Thành</span>
+                </p>
+            </div>
         </div>
-      </div>
-    </footer>
+    </div>
+</footer>
